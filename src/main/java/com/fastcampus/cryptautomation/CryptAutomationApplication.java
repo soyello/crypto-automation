9014fab080ec031e1b70658e818b1cfbc8e3f49f
soyello <wodus9193@naver.com>
2025-08-20 00:41:17 +0900

    github actions

--- src/main/java/com/fastcampus/cryptautomation/CryptAutomationApplication.java
+++ src/main/java/com/fastcampus/cryptautomation/CryptAutomationApplication.java
@@ -2,12 +2,14 @@ package com.fastcampus.cryptautomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CryptAutomationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CryptAutomationApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(CryptAutomationApplication.class, args);
+        SpringApplication.exit(run);
     }
 
 }
